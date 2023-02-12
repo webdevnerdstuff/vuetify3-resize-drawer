cd5c27a47be456dbdecff1ad99755324d9e61852
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2023-02-11 19:25:13 -0800

    Adding component templates

---
+++ src/components/Menu.vue
@@ -0,0 +1,19 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+import { computed, onMounted, reactive, ref, watch } from 'vue';
+
+export default {
+	name: 'MenuComponent',
+	emits: [],
+	props: {},
+	setup(props, { emit }) {
+		return {
+		};
+	},
+};
+</script>
+
+<style lang="scss"></style>
