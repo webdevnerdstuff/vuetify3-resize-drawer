9deaab605bbe5bcfe68966a0c8ac9b52ad398632
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2023-02-11 19:08:57 -0800

    Adding entry files

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { createApp } from 'vue';
+import '@/style.css';
+import App from '@/App.vue';
+import { registerPlugins } from '@/plugins';
+
+const app = createApp(App);
+
+registerPlugins(app);
+
+app.mount('#app');
