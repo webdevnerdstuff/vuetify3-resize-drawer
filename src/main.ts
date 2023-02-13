b07856b3e2a8d019546a707fcf9bdb118afd2925
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2023-02-12 19:43:13 -0800

    Includeing resize drawer component

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import { createApp } from 'vue';
 import '@/style.css';
 import App from '@/App.vue';
 import { registerPlugins } from '@/plugins';
+import VResizeDrawer from '@/index';
 
 const app = createApp(App);
+app.use(VResizeDrawer);
 
 registerPlugins(app);
 
