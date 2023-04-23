173c26ef1597749d453d4b365a1ea279db3e4150
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2023-04-22 20:27:18 -0700

    Updating and adding types

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,28 +1,5 @@
-export interface DrawerOptions {
-	absolute?: boolean;
-	dark?: boolean;
-	elevation?: string | number | undefined;
-	expandOnHover?: boolean;
-	floating?: boolean;
-	handleBorderWidth?: string | number | undefined;
-	handleColor?: object;
-	handlePosition?: string;
-	height?: string | number | undefined;
-	image?: string;
-	name?: string;
-	rail?: boolean;
-	railWidth?: string | number | undefined;
-	resizable?: boolean;
-	saveWidth?: boolean;
-	sticky?: boolean;
-	storageName?: string;
-	storageType?: string;
-	tag?: string;
-	theme?: string | undefined;
-	temporary?: boolean;
-	width?: string | number | undefined;
-	modelValue?: boolean;
-}
+import type { PropType } from 'vue';
+
 
 export interface DrawerClasses {
 	'v-navigation-drawer--absolute'?: boolean;
@@ -38,3 +15,149 @@ export interface DrawerClasses {
 	'v-navigation-drawer--right'?: boolean;
 	'v-navigation-drawer--temporary'?: boolean;
 }
+
+export type WidthProp = number | string | undefined;
+
+export type LocationPropType = PropType<"end" | "start" | "left" | "top" | "bottom" | "right">;
+export type StorageTypePropType = PropType<"local" | "session">;
+
+
+// -------------------------------------------------- Props //
+export type Props = {
+	absolute?: {
+		default: boolean;
+		required: boolean;
+		type: PropType<boolean>;
+	};
+	color?: {
+		default: string | undefined;
+		required: boolean;
+		type: PropType<string>;
+	};
+	dark?: {
+		default: boolean;
+		required: boolean;
+		type: PropType<boolean>;
+	};
+	elevation?: {
+		default: number | string | undefined;
+		required: boolean;
+		type: PropType<number | string | undefined>;
+	};
+	expandOnHover?: {
+		default: boolean;
+		required: boolean;
+		type: PropType<boolean>;
+	};
+	floating?: {
+		default: boolean;
+		required: boolean;
+		type: PropType<boolean>;
+	};
+	handleBorderWidth?: {
+		default: number | string | undefined;
+		required: boolean;
+		type: PropType<number | string | undefined>;
+	};
+	handleColor?: {
+		default: object;
+		required: boolean;
+		type: PropType<object>;
+	};
+	handlePosition?: {
+		default: string;
+		required: boolean;
+		type: PropType<string>;
+	};
+	height?: {
+		default: number | string | undefined;
+		required: boolean;
+		type: PropType<number | string | undefined>;
+	};
+	image?: {
+		default: string;
+		required: boolean;
+		type: PropType<string>;
+	};
+	location?: {
+		default: string;
+		required: boolean;
+		type: LocationPropType;
+	};
+	name?: {
+		default: string;
+		required: boolean;
+		type: PropType<string>;
+	};
+	rail?: {
+		default: boolean;
+		required: boolean;
+		type: PropType<boolean>;
+	};
+	railWidth?: {
+		default: number | string | undefined;
+		required: boolean;
+		type: PropType<number | string | undefined>;
+	};
+	resizable?: {
+		default: boolean;
+		required: boolean;
+		type: PropType<boolean>;
+	};
+	saveWidth?: {
+		default: boolean;
+		required: boolean;
+		type: PropType<boolean>;
+	};
+	sticky?: {
+		default: boolean;
+		required: boolean;
+		type: PropType<boolean>;
+	};
+	storageName?: {
+		default: string;
+		required: boolean;
+		type: PropType<string>;
+	};
+	storageType?: {
+		default: string;
+		required: boolean;
+		type: StorageTypePropType;
+	};
+	tag?: {
+		default: string;
+		required: boolean;
+		type: PropType<string>;
+	};
+	theme?: {
+		default: string | undefined;
+		required: boolean;
+		type: PropType<string | undefined>;
+	};
+	temporary?: {
+		default: boolean;
+		required: boolean;
+		type: PropType<boolean>;
+	};
+	width?: {
+		default: number | string | undefined;
+		required: boolean;
+		type: PropType<WidthProp>;
+	};
+	modelValue?: {
+		default: boolean;
+		required: boolean;
+		type: PropType<boolean>;
+	};
+};
+
+
+// -------------------------------------------------- Storage //
+export type SetStorage = {
+	action?: string;
+	resizedWidth?: number | string;
+	storageType?: string;
+	storageName?: string;
+	saveWidth?: boolean;
+	rail?: boolean;
+};
